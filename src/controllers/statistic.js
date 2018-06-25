94870a055ea00561d8e0adc80842fe68b3894698
Alfonso Delgado <adelgado@gstrackme.com>
2018-06-25 01:47:25 -0500

    Statistics by zones

--- src/controllers/statistic.js
+++ src/controllers/statistic.js
@@ -31,11 +31,18 @@ controller.sectionsStatistics = (req, res) => {
     });
 }
 
+/**
+ * Obtener estadística de las casillas
+ * 
+ * @param req
+ * @param res
+ * 
+ * @returns statistics
+ */
 controller.boxesStatistics = (req, res) => {
     const {section_id} = req.query;
 
-    var stm = `SELECT 
-    b.box_id,
+    var stm = `SELECT b.box_id,
     CONCAT('CASILLA ', b.box_id, ' - ', b.type, ' - ', bu.description) AS description,
     COUNT(v.voter_id) AS total, 
     SUM(IF(v.attended = 1, 1, 0)) AS total_assists,
@@ -56,6 +63,34 @@ controller.boxesStatistics = (req, res) => {
         res.status(500).send(err);
     });
 }
+
+/**
+ * Obtener estadística de las zonas
+ * 
+ * @param req
+ * @param res
+ * 
+ * @returns statistics
+ */
+controller.zonesStatistics = (req, res) => {
+    var stm = `SELECT z.zone_id,z.description,COUNT(v.voter_id) AS total, 
+    SUM(IF(v.attended = 1, 1, 0)) AS total_assists,
+    ROUND((SUM(IF(v.attended  = 1, 1 , 0 )))*100 /COUNT(v.voter_id), 1 ) AS percentage_assists,
+    SUM(IF(v.attended = 0, 1, 0)) AS total_absences,
+    ROUND((SUM(IF(v.attended  = 0, 1 , 0 )))*100 /COUNT(v.voter_id), 1 ) AS percentage_absences
+    FROM voters v
+    INNER JOIN boxes b ON v.box_id = b.box_id
+    INNER JOIN sections s ON b.section_id = s.section_id
+    INNER JOIN zones z ON s.zone_id = z.zone_id
+    GROUP BY z.description
+    ORDER BY z.zone_id;`;
+    
+    db.votes_app.query(stm).then(statistics => {
+        res.status(200).send(statistics);
+    }).catch((err) =>{
+        res.status(500).send(err);
+    });
+};
 
 /* Export module */
 module.exports = controller;
