2142a72fb7664763afc7a4742e2ca45bfa489b84
Alfonso Delgado <adelgado@gstrackme.com>
2018-06-25 01:41:24 -0500

    Statistics by boxes

--- src/controllers/statistic.js
+++ src/controllers/statistic.js
@@ -12,7 +12,7 @@ const controller = {};
  * 
  * @returns statistics
  */
-controller.sectionStatistics = (req, res) => {
+controller.sectionsStatistics = (req, res) => {
     var stm = `SELECT s.section_id,s.description,COUNT(v.voter_id) AS total, 
     SUM(IF(v.attended = 1, 1, 0)) AS total_assists,
     ROUND((SUM(IF(v.attended  = 1, 1 , 0 )))*100 /COUNT(v.voter_id), 1 ) AS percentage_assists,
@@ -23,6 +23,32 @@ controller.sectionStatistics = (req, res) => {
     INNER JOIN sections s ON b.section_id = s.section_id
     GROUP BY s.description
     ORDER BY s.section_id;`;
+
+    db.votes_app.query(stm).then(statistics => {
+        res.status(200).send(statistics);
+    }).catch((err) =>{
+        res.status(500).send(err);
+    });
+}
+
+controller.boxesStatistics = (req, res) => {
+    const {section_id} = req.query;
+
+    var stm = `SELECT 
+    b.box_id,
+    CONCAT('CASILLA ', b.box_id, ' - ', b.type, ' - ', bu.description) AS description,
+    COUNT(v.voter_id) AS total, 
+    SUM(IF(v.attended = 1, 1, 0)) AS total_assists,
+    ROUND((SUM(IF(v.attended  = 1, 1 , 0 )))*100 /COUNT(v.voter_id), 1 ) AS percentage_assists,
+    SUM(IF(v.attended = 0, 1, 0)) AS total_absences,
+    ROUND((SUM(IF(v.attended  = 0, 1 , 0 )))*100 /COUNT(v.voter_id), 1 ) AS percentage_absences
+    FROM voters v
+    INNER JOIN boxes b ON v.box_id = b.box_id
+    INNER JOIN sections s ON b.section_id = s.section_id
+    INNER JOIN box_ubications bu ON b.box_ubication_id = bu.box_ubication_id
+    WHERE b.section_id = ${section_id}
+    GROUP BY CONCAT('CASILLA ', b.box_id, ' - ', b.type, ' - ', bu.description)
+    ORDER BY b.box_id;`;
 
     db.votes_app.query(stm).then(statistics => {
         res.status(200).send(statistics);
--- src/routes/statistic.js
+++ src/routes/statistic.js
@@ -3,7 +3,8 @@
 const router = require('express').Router();
 const statistic = require('../controllers/statistic');
 
-router.get('/statistics/sections', statistic.sectionStatistics);
+router.get('/statistics/sections', statistic.sectionsStatistics);
+router.get('/statistics/boxes', statistic.boxesStatistics);
 
 /* Export module */
 module.exports = router;
