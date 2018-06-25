c523ae46cce580ae8f0a3c5ddc09a7c9501206ca
Alfonso Delgado <adelgado@gstrackme.com>
2018-06-25 01:48:51 -0500

    Statistics by zones v1

--- src/routes/statistic.js
+++ src/routes/statistic.js
@@ -5,6 +5,7 @@ const statistic = require('../controllers/statistic');
 
 router.get('/statistics/sections', statistic.sectionsStatistics);
 router.get('/statistics/boxes', statistic.boxesStatistics);
+router.get('/statistics/zones', statistic.zonesStatistics);
 
 /* Export module */
 module.exports = router;
