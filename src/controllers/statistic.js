c12ede93d7956f1fb84dc023adbfc5e525333672
Alfonso Delgado <adelgado@gstrackme.com>
2018-06-25 01:29:33 -0500

    Statistics by sections

--- src/controllers/statistic.js
+++ src/controllers/statistic.js
@@ -4,5 +4,32 @@ const db = require('../config/db.config');
 
 const controller = {};
 
+/**
+ * Obtener estadística de las secciones
+ * 
+ * @param req
+ * @param res
+ * 
+ * @returns statistics
+ */
+controller.sectionStatistics = (req, res) => {
+    var stm = `SELECT s.section_id,s.description,COUNT(v.voter_id) AS total, 
+    SUM(IF(v.attended = 1, 1, 0)) AS total_assists,
+    ROUND((SUM(IF(v.attended  = 1, 1 , 0 )))*100 /COUNT(v.voter_id), 1 ) AS percentage_assists,
+    SUM(IF(v.attended = 0, 1, 0)) AS total_absences,
+    ROUND((SUM(IF(v.attended  = 0, 1 , 0 )))*100 /COUNT(v.voter_id), 1 ) AS percentage_absences
+    FROM voters v
+    INNER JOIN boxes b ON v.box_id = b.box_id
+    INNER JOIN sections s ON b.section_id = s.section_id
+    GROUP BY s.description
+    ORDER BY s.section_id;`;
+
+    db.votes_app.query(stm).then(statistics => {
+        res.status(200).send(statistics);
+    }).catch((err) =>{
+        res.status(500).send(err);
+    });
+}
+
 /* Export module */
 module.exports = controller;
--- src/routes/statistic.js
+++ src/routes/statistic.js
@@ -3,5 +3,7 @@
 const router = require('express').Router();
 const statistic = require('../controllers/statistic');
 
+router.get('/statistics/sections', statistic.sectionStatistics);
+
 /* Export module */
 module.exports = router;
