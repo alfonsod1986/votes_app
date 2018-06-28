0a04baa18ca692f5f75cb26fb174a59a40385c6f
Alfonso Delgado <adelgado@gstrackme.com>
2018-06-28 15:16:11 -0500

    Boxes Statistics v2

--- src/controllers/statistic.js
+++ src/controllers/statistic.js
@@ -34,7 +34,31 @@ controller.boxesStatistics = (req, res) => {
     var stm = `CALL sp_get_statistics_by_boxes();`;
 
     db.votes_app.query(stm).then(statistics => {
-        res.status(200).send(statistics);
+
+        var sections = [];
+
+        statistics.forEach(outer => {
+            var boxes = [];
+            var section_id = outer.section_id;
+
+            statistics.forEach(inner =>{
+                if(section_id == inner.section_id){
+                    boxes.push({
+                        description: inner.box_description,
+                        total: inner.total,
+                        total_assists: inner.total_assists,
+                        percentage_assists: inner.percentage_assists,
+                        total_absences: inner.total_absences,
+                        percentage_absences: inner.percentage_absences
+                    });
+                }
+            });
+            sections.push({
+                description: outer.description,
+                boxes: boxes
+            });
+        });
+        res.status(200).send(sections);
     }).catch((err) =>{
         res.status(500).send(err);
     });
